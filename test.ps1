373e77c1d9a8c44cf12fd36137eecb762d8295ee
Ken Ward <kenrward@gmail.com>
2023-04-26 10:08:39 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,2 @@
 Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n10.0.0.4`tzndemo.com" -Force
-Set-DNSClientServerAddress "Ethernet" –ServerAddresses ("10.0.0.1","8.8.8.8")
+Set-DNSClientServerAddress "Ethernet" –ServerAddresses ("10.0.0.4","8.8.8.8")
